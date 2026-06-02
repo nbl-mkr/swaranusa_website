99713c8d9268db54bf8b8c15d30c7f3a3387ad0f
MNM29 <n481lmaka@gmail.com>
2026-06-03 03:36:45 +0700

    commit

--- navbar_menu.php
+++ navbar_menu.php
@@ -1,6 +1,6 @@
 <nav id="navMenu">
     <ul>
-        <li><a href="landing.php" onclick="closeMenu()">Beranda</a></li>
+        <li><a href="index.php" onclick="closeMenu()">Beranda</a></li>
         <li><a href="jelajahi.php" onclick="closeMenu()">Jelajahi</a></li>
         <li><a href="belajar.php" onclick="closeMenu()">Belajar</a></li>
         <li><a href="tentang.php" onclick="closeMenu()">Tentang</a></li>
